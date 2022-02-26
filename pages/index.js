833955f560566362040e2996f4b435013ae9b666
sayed khalil <34566021+sayedkhalil@users.noreply.github.com>
2022-02-26 05:08:11 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+   
 import Head from 'next/head'
 import Image from 'next/image'
 import About from '../layout/about'
@@ -8,7 +9,7 @@ import Partener from '../layout/partener'
 import Productes from '../layout/productes'
 import styles from '../styles/Home.module.css'
 import { db, storage } from "../firebase";
-import { collection, addDoc ,getDocs,doc,Timestamp,deleteDoc , setDoc,getDoc} from "firebase/firestore";
+import { collection, addDoc ,getDocs,doc,Timestamp,deleteDoc , setDoc,getDoc, query, where} from "firebase/firestore";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 export default function Home({getdata}) {
@@ -23,7 +24,7 @@ export default function Home({getdata}) {
         <link rel="icon" href={getdata.getinfo.logo} type="image/x-icon" />
       </Head>
           <Cover data={getdata.getcov} />
-          <NEw data={getdata.products} />
+          <NEw data={getdata.getnew} />
           <About data={getdata.getinfo}/>
           <Productes data={getdata.products} />
           <Partener data={getdata.getpart}/>
@@ -33,16 +34,17 @@ export default function Home({getdata}) {
   )
 }
 export async function getServerSideProps(){
-  const de=[]  
+  const de=[]
   const opnionarr=[]
   const pro=[]
+  const neww =[]
   const infoRef = doc(db, "info", "info");
   const infoSnap = await getDoc(infoRef)
   const getinfo =  infoSnap.data()?infoSnap.data().info:{}
   const docRef = doc(db, "cover", "cover");
   const docSnap = await getDoc(docRef);
   const getcover =  docSnap.data()?docSnap.data().covers:[]
-  const docRefpar = doc(db, "partn", "partn"); 
+  const docRefpar = doc(db, "partn", "partn");
   const docSnapar = await getDoc(docRefpar);
   const getpartn =  docSnapar.data()?docSnapar.data().partns:[]
   const codelist = collection(db, 'category');
@@ -57,8 +59,13 @@ export async function getServerSideProps(){
   const getopnion =async()=> opnionsnap?opnionsnap.docs.map(doc =>{opnionarr.push(doc.data());   }):[];
   products()
   getopnion()  
- 
+  const newlist = collection(db, 'broductes');
+  const n= query(newlist, where("category", "==","أبواب حديد"));
+  const nprodsnapshot= await getDocs(n);
+   nprodsnapshot?nprodsnapshot.forEach(doc =>{ neww.push({code:doc.data().code,
+      title:doc.data().title,category:doc.data().category,imges:doc.data().imges[0]})  }):neww
      return{
-      props:{getdata:{data:de,products:pro,getcov:getcover,getpart:getpartn,getopn:opnionarr,getinfo:getinfo}}
+      props:{getdata:{data:de,products:pro,getcov:getcover,getpart:getpartn,getopn:opnionarr,getinfo:getinfo,getnew:neww}}
            }
 }
+
